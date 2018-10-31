d8500c44a6ab0a8b7ae81e10d3b70388e8a3d9b4
Ben Tarrant <ben.g.tarrant@gmail.com>
2018-10-31 16:53:31 +0000

    Simple Scene Start
    Simple scene with pro builder and pro grids installed.
    
    simple capsule that uses a nav mesh to travel to a location that's clicked on the mesh.
    
    next step is to allow this to sync/work across the network

---
+++ Cheddar Champions/Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+
+public class PlayerController : MonoBehaviour {
+
+    public Camera IsoCam;
+    public NavMeshAgent Agent;
+
+    void Start()
+    {
+        //if (!isLocalPlayer)
+        {
+            transform.Find("Capsule").gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = IsoCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Move the Player
+
+                Agent.SetDestination(hit.point);
+            }
+        }
+	}
+}
