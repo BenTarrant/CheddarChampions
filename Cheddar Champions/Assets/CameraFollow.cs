8339f5c407d364c2fd9880ed832970688fde3d03
Ben Tarrant <ben.g.tarrant@gmail.com>
2018-11-22 16:59:42 +0000

    Cheese Goes Online
    Now able to spawn multiple players over network (LAN)
    
    Began work on pressure pads - current denotes how many colliders are present using Overlap Box
    
    NOT WORKING - need to limit the amount of checks per frame/seconds (Coroutine solution)

---
+++ Cheddar Champions/Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    //public int depth = -20;
+
+    void Update()
+    {
+        if (playerTransform != null)
+        {
+            transform.position = playerTransform.position;
+        }
+    }
+
+    public void setTarget(Transform target)
+    {
+        playerTransform = target;
+    }
+}
+
--- Cheddar Champions/Assets/PlayerController.cs
+++ Cheddar Champions/Assets/PlayerController.cs
@@ -1,47 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour
+{
 
-    [SerializeField]
-
-    float moveSpeed = 4f;
-
+    [SerializeField] private float moveSpeed = 4f;
     Vector3 forward, right;
 
+    public override void OnStartLocalPlayer()
+    {
+        Camera.main.GetComponent<CameraFollow>().setTarget(gameObject.transform);
+    }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            Destroy(transform.Find("Camera Target").gameObject);
+            transform.Find("MouseLOD0").gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+            return;
+        }
 
         forward = Camera.main.transform.forward; // set forward vector to equal camera's forward vector
         forward.y = 0; //ensure our y value always set to 0
         forward = Vector3.Normalize(forward); //making sure the vector is set to 1 for motion
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.anyKey) // although any key declared, only WASD assigned the values necessary
         {
             Move(); // move the player
         }
-		
-	}
+    }
 
     void Move()
     {
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); // new direction equals values specified in input manager
+        //Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); // new direction equals values specified in input manager
+        //THIS CODE EXISTS TO ENABLE MOBILE INPUT COMMANDS
+
+
         Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
         Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
 
         Vector3 heading = Vector3.Normalize(rightMovement + upMovement); //combines and normalises above movment to allow player to have a heading during movement
 
-        transform.forward = heading; // sets the rotation of the player to be where the input Vector3 dictates
-        transform.position += rightMovement; // sets the left/right movement of the player to be where the input Vector3 dictates
-        transform.position += upMovement; // sets the up/down movement of the player to be where the input Vector3 dictates
+        GetComponent<CharacterController>().SimpleMove(heading * moveSpeed);
 
+        transform.forward = heading; // sets the rotation of the player to be where the input Vector3 dictates
+        //transform.position += rightMovement; // sets the left/right movement of the player to be where the input Vector3 dictates
+        //transform.position += upMovement; // sets the up/down movement of the player to be where the input Vector3 dictates
     }
 }
