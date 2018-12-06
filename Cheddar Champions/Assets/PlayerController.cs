92d19b0a081fd239aa832d77e971c9763a9e64cc
Ben Tarrant <ben.g.tarrant@gmail.com>
2018-12-06 15:59:56 +0000

    Camera Fix
    Attempted to fix camera issues - identified reflection issues with orthographic camera.
    
    Perspective camera implementation started but not currently working.

--- Cheddar Champions/Assets/PlayerController.cs
+++ Cheddar Champions/Assets/PlayerController.cs
@@ -7,9 +7,16 @@ public class PlayerController : NetworkBehaviour
     [SerializeField] private float moveSpeed = 4f;
     Vector3 forward, right;
 
+    public float VerticalSpeed = 10f;
+    public float HorizontalSpeed = 60f;
+    public float rotationSpeed = 5f;
+    private Transform myTransform;
+
+    private CharacterController Cc;
+
     public override void OnStartLocalPlayer()
     {
-        Camera.main.GetComponent<CameraFollow>().setTarget(gameObject.transform);
+        //Camera.main.GetComponent<CameraFollow>().setTarget(gameObject.transform);
     }
 
     // Use this for initialization
@@ -23,35 +30,56 @@ public class PlayerController : NetworkBehaviour
             return;
         }
 
-        forward = Camera.main.transform.forward; // set forward vector to equal camera's forward vector
-        forward.y = 0; //ensure our y value always set to 0
-        forward = Vector3.Normalize(forward); //making sure the vector is set to 1 for motion
-        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+       Cc = GetComponent<CharacterController>();
+        myTransform = transform;
+
+        //forward = Camera.main.transform.forward; // set forward vector to equal camera's forward vector
+        //forward.y = 0; //ensure our y value always set to 0
+        //forward = Vector3.Normalize(forward); //making sure the vector is set to 1 for motion
+        //right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey) // although any key declared, only WASD assigned the values necessary
+        if (Input.anyKey)
         {
-            Move(); // move the player
+            Move();
         }
+  
+        //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        //Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        //Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        //Vector3 heading = Vector3.Normalize(rightMovement + upMovement); //combines and normalises above movment to allow player to have a heading during movement
+
+        //Cc.Move(heading * Time.deltaTime * moveSpeed);
     }
 
     void Move()
     {
+        float horizontal = Input.GetAxis("Horizontal") * HorizontalSpeed;
+        float vertical = Input.GetAxis("Vertical") * VerticalSpeed;
+        Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
+
+        Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+        myTransform.rotation = Quaternion.Slerp(myTransform.rotation, newRotation, rotationSpeed * Time.deltaTime);
+
+        // CharacterController.Move to move the player in target direction
+        Cc.Move(moveDirection * Time.deltaTime);
+
+
+
         //Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); // new direction equals values specified in input manager
         //THIS CODE EXISTS TO ENABLE MOBILE INPUT COMMANDS
 
 
-        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
-        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
 
-        Vector3 heading = Vector3.Normalize(rightMovement + upMovement); //combines and normalises above movment to allow player to have a heading during movement
 
-        GetComponent<CharacterController>().SimpleMove(heading * moveSpeed);
+        //GetComponent<CharacterController>().SimpleMove(heading * moveSpeed);
 
-        transform.forward = heading; // sets the rotation of the player to be where the input Vector3 dictates
+        //transform.forward = heading; // sets the rotation of the player to be where the input Vector3 dictates
         //transform.position += rightMovement; // sets the left/right movement of the player to be where the input Vector3 dictates
         //transform.position += upMovement; // sets the up/down movement of the player to be where the input Vector3 dictates
     }
