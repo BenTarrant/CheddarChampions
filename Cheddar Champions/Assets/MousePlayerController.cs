db5a7a94e39f36ae2edabb7cac455ab0e4e62450
Ben Tarrant <ben.g.tarrant@gmail.com>
2019-01-06 14:16:48 +0000

    Initial Mobile Support
    ADDED: iOS and Android support packages to Untiy/ Project
    
    ADDED: Joystick movement
    
    ADDED: joystick UI
    
    ADDED: Button for 'cheese eating'
    
    FIXED: Animation now works with new joystick-based movement.
    
    FIXED: Both players now appear correctly across network and inputs work on mobile device

--- Cheddar Champions/Assets/MousePlayerController.cs
+++ Cheddar Champions/Assets/MousePlayerController.cs
@@ -11,6 +11,8 @@ public class MousePlayerController : NetworkBehaviour {
    Animator PlayerAnim;
     bool bl_walk = false;
 
+    public Joystick joystick = null;
+
     public override void OnStartLocalPlayer()
     {
         Camera.main.GetComponentInParent<CameraFollow>().setTarget(gameObject.transform);
@@ -23,44 +25,50 @@ public class MousePlayerController : NetworkBehaviour {
             if (!isLocalPlayer)
             {
                 //Destroy(transform.Find("CameraTarget").gameObject);
-                //transform.Find("MouseLOD0").gameObject.GetComponent<Renderer>().material.color = Color.red;
+                //transform.Find("MouseLOD0").gameObject.GetComponentInChildren<Renderer>().material.color = Color.red;
 
                 return;
             }
 
+        GameObject Fixedjoystick = GameObject.FindGameObjectWithTag("Joystick") as GameObject;
+        joystick = Fixedjoystick.GetComponent<Joystick>();
 
-        forward = Camera.main.transform.forward; // set forward vector to equal camera's forward vector
-        forward.y = 0; //ensure our y value always set to 0
-        forward = Vector3.Normalize(forward); //making sure the vector is set to 1 for motion
-        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward; // creates a rotation for our right vector, rotated 90 degrees around the x axis.
+        //forward = Camera.main.transform.forward; // set forward vector to equal camera's forward vector
+        //forward.y = 0; //ensure our y value always set to 0
+        //forward = Vector3.Normalize(forward); //making sure the vector is set to 1 for motion
+        //right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward; // creates a rotation for our right vector, rotated 90 degrees around the x axis.
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(isLocalPlayer)
+
+        if (isLocalPlayer)
         {
             PlayerAnim.SetBool("bl_walk", false);
 
-            if (Input.GetAxis("HorizontalKey") != 0 || (Input.GetAxis("VerticalKey") != 0))
+            Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.forward * joystick.Vertical);
+
+            if (moveVector != Vector3.zero)
             {
-                Move();
                 PlayerAnim.SetBool("bl_walk", true);
+                transform.rotation = Quaternion.LookRotation(moveVector);
+                transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
             }
         }
 
     }
 
-    void Move()
-    {
-        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey")); // new direction equals values specified in input manager
-        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
-        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+    //void PC_Move()
+    //{
+    //    Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey")); // new direction equals values specified in input manager
+    //    Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+    //    Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
 
-        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
-        transform.forward = heading;
-        transform.position += rightMovement; // sets the left/right movement of the player to be where the input Vector3 dictates
-        transform.position += upMovement; // sets the up/down movement of the player to be where the input Vector3 dictates
+    //    Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+    //    transform.forward = heading;
+    //    transform.position += rightMovement; // sets the left/right movement of the player to be where the input Vector3 dictates
+    //    transform.position += upMovement; // sets the up/down movement of the player to be where the input Vector3 dictates
 
-    }
+    //}
 }
---
+++ Cheddar Champions/Assets/Virtual Joystick Pack/Examples/3D Example/Player3DExample.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Player3DExample : MonoBehaviour {
+
+    public float moveSpeed = 8f;
+    public Joystick joystick;
+
+	void Update () 
+	{
+        
+	}
+}
