9fad1022ba1150f20f30f7e2c87e27c568dfac10
Ben Tarrant <ben.g.tarrant@gmail.com>
2019-01-06 17:43:10 +0000

    Catastrophe Averted
    Managed to revert project back to previous state prior to broken build.
    
    ADDED: Button press functionality ready for a cheese-eating method

--- Cheddar Champions/Assets/Scripts/MousePlayerController.cs
+++ Cheddar Champions/Assets/Scripts/MousePlayerController.cs
@@ -11,6 +11,8 @@ public class MousePlayerController : NetworkBehaviour {
    Animator PlayerAnim;
     //bool bl_walk = false;
 
+    public Joystick joystick = null;
+
     public override void OnStartLocalPlayer()
     {
         Camera.main.GetComponentInParent<CameraFollow>().setTarget(gameObject.transform);
@@ -22,50 +24,53 @@ public class MousePlayerController : NetworkBehaviour {
 
             if (!isLocalPlayer)
             {
-<<<<<<< HEAD:Cheddar Champions/Assets/Scripts/MousePlayerController.cs
+
                 //Destroy(GameObject.Find("Canvas").gameObject);
                 //transform.Find("MouseLOD0").gameObject.GetComponentInChildren<Renderer>().material.color = Color.red;
-=======
-                //Destroy(transform.Find("CameraTarget").gameObject);
-                //transform.Find("MouseLOD0").gameObject.GetComponent<Renderer>().material.color = Color.red;
->>>>>>> parent of db5a7a9... Initial Mobile Support:Cheddar Champions/Assets/MousePlayerController.cs
 
                 return;
             }
 
+        GameObject Fixedjoystick = GameObject.FindGameObjectWithTag("Joystick") as GameObject;
+        joystick = Fixedjoystick.GetComponent<Joystick>();
 
-        forward = Camera.main.transform.forward; // set forward vector to equal camera's forward vector
-        forward.y = 0; //ensure our y value always set to 0
-        forward = Vector3.Normalize(forward); //making sure the vector is set to 1 for motion
-        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward; // creates a rotation for our right vector, rotated 90 degrees around the x axis.
+        //forward = Camera.main.transform.forward; // set forward vector to equal camera's forward vector
+        //forward.y = 0; //ensure our y value always set to 0
+        //forward = Vector3.Normalize(forward); //making sure the vector is set to 1 for motion
+        //right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward; // creates a rotation for our right vector, rotated 90 degrees around the x axis.
 
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         if(isLocalPlayer)
         {
             PlayerAnim.SetBool("bl_walk", false);
 
-            if (Input.GetAxis("HorizontalKey") != 0 || (Input.GetAxis("VerticalKey") != 0))
+            Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.forward * joystick.Vertical);
+
+            if (moveVector != Vector3.zero)
             {
-                Move();
+                //Move();
                 PlayerAnim.SetBool("bl_walk", true);
+                transform.rotation = Quaternion.LookRotation(moveVector);
+                transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
             }
         }
 
     }
 
-    void Move()
-    {
-        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey")); // new direction equals values specified in input manager
-        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
-        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+    //void Move()
+    //{
+    //    Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey")); // new direction equals values specified in input manager
+    //    Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+    //    Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
 
-        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
-        transform.forward = heading;
-        transform.position += rightMovement; // sets the left/right movement of the player to be where the input Vector3 dictates
-        transform.position += upMovement; // sets the up/down movement of the player to be where the input Vector3 dictates
+    //    Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+    //    transform.forward = heading;
+    //    transform.position += rightMovement; // sets the left/right movement of the player to be where the input Vector3 dictates
+    //    transform.position += upMovement; // sets the up/down movement of the player to be where the input Vector3 dictates
 
-    }
+    //}
 }
